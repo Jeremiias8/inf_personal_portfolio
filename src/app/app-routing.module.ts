bfb84eda374219a9828ec44c0a1f2f2f7679672c
Jeremías <jeremiias122002@gmail.com>
2023-07-25 18:15:13 +0200

    some-styling-changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+/* components */
+import { AppComponent } from './app.component';
+
+import { PersonalInfoComponent } from './components/personal-info/personal-info.component';
+import { ExperienceComponent } from './components/experience/experience.component';
+import { RrssComponent } from './components/rrss/rrss.component';
+import { LanguagesComponent } from './components/languages/languages.component';
+import { ErrorComponent } from './components/error/error.component';
+
+
+const routes: Routes = [
+  { path: 'personal-info', component: PersonalInfoComponent },
+  { path: 'experience', component: ExperienceComponent },
+  { path: 'rrss', component: RrssComponent },
+  { path: 'languages', component: LanguagesComponent },
+  { path: '**', component: ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { PersonalInfoComponent } from './components/personal-info/personal-info.component';
+import { RrssComponent } from './components/rrss/rrss.component'
+import { ExperienceComponent } from './components/experience/experience.component';
+import { ErrorComponent } from './components/error/error.component';
+import { LanguagesComponent } from './components/languages/languages.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PersonalInfoComponent,
+    RrssComponent,
+    ExperienceComponent,
+    ErrorComponent,
+    LanguagesComponent
   ],
   imports: [
     BrowserModule,
